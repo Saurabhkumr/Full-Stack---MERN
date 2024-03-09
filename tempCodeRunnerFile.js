ea2f45cc03d2bc96e4bac2df5b18677046b9522b
Saurabh <saurabhkr0927@gmail.com>
2024-03-09 12:33:34 +0530

    array ques

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-
+let arr6 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr6.length; i++) {
+//   if (arr6[i] % 2 == 0) {
+//     console.log(`${arr6[i]} Even no`);
+//   } else {
+//     console.log(`${arr6[i]} Odd No`);
+//   }
+// }
--- test.js
+++ test.js
@@ -61,7 +61,7 @@
 // str = str.split(' ').map(word=>word.split('').reverse().join('')).reverse().join(' ');
 // console.log(str);
 
-// Q4
+// Q5
 
 // for(let i = 1 ; i<=6 ; i++){
 //   for(let j = 6-i ; j>0;j--){
@@ -78,7 +78,7 @@
 
 // }
 
-//Q5
+//Q6
 
 // function fact(num){
 //   if(num==0) return 1;
@@ -96,25 +96,120 @@
 //   console.log()
 // }
 
-// Q6
+// Q7
 
-let str = "saurabhkumar";
-let a = 0;
-let n = str.length;
-for (let i = 1; i < n; i++) {
-  for (let k = n; k > 0; k--) {
-    process.stdout.write(" ");
-  }
-  for (let j = i; j > 0; j--) {
-    if (a < str.length) {
-      let str1 = str[a];
-      process.stdout.write(str1 + " ");
-      a++;
-    } else {
-      process.stdout.write(" - ");
-    }
-  }
-  if (a >= str.length) break;
-  n--;
-  console.log();
-}
+// let str = "saurabhkumar";
+// let a = 0;
+// let n = Math.ceil(str.length / 2);
+// for (let i = 1; i < n; i++) {
+//   for (let k = n - i; k > 0; k--) {
+//     process.stdout.write(" ");
+//   }
+//   for (let j = i; j > 0; j--) {
+//     if (a < str.length) {
+//       let str1 = str[a];
+//       process.stdout.write(str1 + " ");
+//       a++;
+//     } else {
+//       process.stdout.write(" - ");
+//     }
+//   }
+//   if (a >= str.length) break;
+//   console.log();
+// }
+
+//Q-8
+
+//i
+
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = arr[i] + 2;
+// }
+// console.log(arr);
+
+//ii
+// let arr2 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr2.length; i++) {
+//   arr2[i] = arr2[i] - 2;
+// }
+// console.log(arr2);
+
+//iii
+// let arr3 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr3.length; i++) {
+//   arr3[i] = arr3[i] ** 2;
+// }
+// console.log(arr3);
+
+//iv
+// let arr4 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr4.length; i++) {
+//   arr4[i] = 1;
+// }
+// console.log(arr4);
+
+//v
+// let arr5 = [1, 121, 13, 241, 51];
+// arr5.forEach((element) => {
+//   prime(element);
+// });
+// function prime(num) {
+//   let a = Math.ceil(Math.sqrt(num));
+//   let flag = 0;
+//   if (num <= 1) {
+//     console.log(`${num} - Neither prime or non prime`);
+//     return;
+//   }
+
+//   for (let i = 2; i <= a; i++) {
+//     if (num % i == 0) {
+//       flag = 1;
+//     }
+//   }
+
+//   if (flag != 0) {
+//     console.log(`${num} - Non Prime`);
+//   } else console.log(`${num} - Prime`);
+// }
+
+//vi
+// let arr6 = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr6.length; i++) {
+//   if (arr6[i] % 2 == 0) {
+//     console.log(`${arr6[i]} Even no`);
+//   } else {
+//     console.log(`${arr6[i]} Odd No`);
+//   }
+// }
+
+//vii
+// let arr7 = [121, 321, 34543, 23, 4445, 55];
+// arr7.forEach((num) => {
+//   palindrome(num);
+// });
+// function palindrome(num) {
+//   let rev = num.toString().split("").reverse().join("");
+//   if (num == rev) console.log(`${num} is a palindrome`);
+//   else console.log(`${num} is not a palindrome`);
+// }
+
+//viii
+// let arr8 = [2, 5, 3, 6, 4];
+// arr8.forEach((num) => {
+//   console.log(`factorial of ${num} is ${factorial(num)}`);
+// });
+// function factorial(num) {
+//   if (num == 0) return 1;
+//   return num * factorial(num - 1);
+// }
+
+//ix
+// let arr9 = [4, 5, 6, 3, 2];
+// arr9.shift();
+// console.log(arr9);
+
+//x
+let arr10 = [2, 3, 4, 5, 6];
+arr10.splice(1, 0, 55, 99);
+console.log(arr10);
